a374f5a987f38fe1384233b5755cee38dd9c0c61
prasadt45 <146625931+prasadt45@users.noreply.github.com>
2025-06-22 12:02:04 +0530

    Add initial implementation of conditional statements and nullish coalescing in JavaScript

---
+++ if_else.js
@@ -0,0 +1,14 @@
+let n= 18 ; 
+if(n>18){
+    console.log("Bhai , you are an adult");
+}else if(n==18){
+    console.log("Bhai , you are just an adult");
+}
+else{
+    console.log("Bhai , you are not an adult");
+}
+
+n>19 ? console.log("Man"):console.log("Boy"); ; 
+
+let age ;
+console.log(age?? "Age is not defined");
