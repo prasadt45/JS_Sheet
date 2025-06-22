dd5aef4098f365bf6ee4b2aee241e340f8c6f7af
prasadt45 <146625931+prasadt45@users.noreply.github.com>
2025-06-22 12:11:29 +0530

    Add loop examples and string iteration in JavaScript

---
+++ loops.js
@@ -0,0 +1,27 @@
+for(let i =0 ; i<10 ; i++){
+    //console.log("The value of i is: " + i);
+}
+
+let i = 0 ;
+while(i<10){
+   //
+   //  console.log(`The value of i is: ${i}`);
+    i++;
+}
+
+let j =0 ; 
+do{
+    //console.log(`The value of j is: ${j}`);
+    j++;
+}while(j<5) ;
+
+let s = "Prasad" ;
+
+for (ch of s){
+    console.log(ch) ;
+}
+let n =  s.length ;
+for(let i =0 ; i<n ; i++){
+    console.log(s[i]) ;
+}
+
